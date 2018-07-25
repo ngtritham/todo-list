1f8e41f61ac3263c40a4f404d545db92cf91b4b7
ngtritham <ngtritham@gmail.com>
2018-07-25 18:20:54 +0700

    Update Views

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,77 @@
-$(document).ready(function () {
-    $('button').click(function () {
-        $('#todo').append("<ul>" + $("input[name=task]").val() + " <a href='#' class='close' aria-hidden='true'>&times;</a></ul>");
-    });
-    $("body").on('click', '#todo a', function () {
-        $(this).closest("ul").remove();
-    });
-});
+// $(document).ready(function () {
+//     $('button').click(function () {
+//         $('#todo').append("<li><i id='abc' class='fas fa-check isChecked'></i>" + $("input[name=task]").val() + "<i class='fas fa-trash-alt close'></i></li>");
+//     });
+//     $("body").on('click', '#todo i', function () {
+//         //$(this).closest("ul").remove();
+//     });
+
+//     $("body").on('click', '#todo i .isChecked', () => {
+//         alert("Checked !!");
+//     });
+
+//     $("li").click(function () {
+//         alert("Click check");
+//     });
+
+//     $("#alert").click(() => {
+//         alert("Click check");
+//     });
+// });
+
+
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
